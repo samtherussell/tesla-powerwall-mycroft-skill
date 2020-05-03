895ba60f95dd6993a305b5231a02b1be50bbc937
samtherussell <samtherussell@tyagrwen.co.uk>
2020-05-03 18:12:06 +0100

    functional with battery charge and power usages

--- README.md
+++ README.md
@@ -1,14 +1,14 @@
 # <img src="https://raw.githack.com/FortAwesome/Font-Awesome/master/svgs/solid/battery-full.svg" card_color="#90251D" width="50" height="50" style="vertical-align:bottom"/> Telsa Powerwall Utility
-Reports tesla powerwall battery status
+Reports tesla powerwall battery stats
 
 ## About
 
 
 ## Examples
-* "What is the battery level"
-* "What is the battery status"
-* "What is the powerwall status"
-* "What is the powerwall level"
+* "how full is the battery"
+* "how much electricty is the house using"
+* "how much power are the solar panels generating"
+* "how much energy is the grid supplying"
 
 ## Credits
 samtherussell
--- __init__.py
+++ __init__.py
@@ -1,15 +1,46 @@
 from mycroft import MycroftSkill, intent_file_handler
+from tesla_powerwall_controller import PowerwallController
 
-
-class TelsaPowerwallUtility(MycroftSkill):
+class TelsaPowerwallSkill(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
+        self.controller = None
 
-    @intent_file_handler('utility.powerwall.telsa.intent')
-    def handle_utility_powerwall_telsa(self, message):
-        self.speak_dialog('utility.powerwall.telsa')
+    def initialize(self):
+      self.settings_change_callback = self.on_settings_changed
+      self.on_settings_changed()
 
+    def on_settings_changed(self):
+        ip_address = self.settings.get('ip_address')
+        self.log.info("read ip address '{ip_address}' from settings")
+        if ip_address:
+          self.controller = PowerwallController(ip_address)
+
+    @intent_file_handler('battery.charge.intent')
+    def battery_charge(self, message):
+        if not self.controller: raise Exception('IP address of tesla powerwall not set')
+        resp = self.controller.get_battery_charge()
+        self.log.info(f"Response: {resp}")
+        self.speak(resp)
+
+    @intent_file_handler('power.level.intent')
+    def power_level(self, message):
+        if not self.controller: raise Exception('IP address of tesla powerwall not set')
+        endpoint = message.data.get('endpoint')
+        self.log.info(f'power.level.intent: {endpoint}')
+        if endpoint == 'battery':
+          resp = self.controller.get_battery_power()
+        elif 'panel' in endpoint:
+          resp = self.controller.get_solar_power()
+        elif endpoint == 'grid':
+          resp = self.controller.get_grid_power()
+        elif endpoint == 'house':
+          resp = self.controller.get_house_power()
+        else:
+          raise Exception(f'invalid endpoint: {endpoint}')
+        self.log.info(f"Response: {resp}")
+        self.speak(resp)
 
 def create_skill():
-    return TelsaPowerwallUtility()
+    return TelsaPowerwallSkill()
 
